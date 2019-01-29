a30413ad3bc933f7e319689283d0e2728572d8b0
Dario Lodi Rizzini <dario@maclavellus.ce.unipr.it>
2019-01-29 18:17:57 +0100

    .

--- include/grd/GlarotSignature.h
+++ include/grd/GlarotSignature.h
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with GRD.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef GRD_GLARE_SIGNATURE_H
-#define GRD_GLARE_SIGNATURE_H
+#ifndef GRD_GLAROT_SIGNATURE_H
+#define GRD_GLAROT_SIGNATURE_H
 
 #include <iostream>
 #include <vector>
